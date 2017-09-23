75ef2c65a4e9f3762723502dd14d795008a59233
an-art <anartyomchik@gmail.com>
2017-09-23 13:10:46 +0300

    fixed joining brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  	var brackets = [];
+    for (var i=0; i<bracketsConfig.length; i++) {
+      brackets[i] = bracketsConfig[i].join('');
+
+  	}
+  	var prev = '';
+  	while (str.length!=prev.length) {
+  		prev = str;
+  		for (var i=0; i<brackets.length; i++) {
+	  		str = str.replace(brackets[i], '');
+	  	}
+	  	
+  	}
+  	return (str.length > 0) ? false : true;
 }
